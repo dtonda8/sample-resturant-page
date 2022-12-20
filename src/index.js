55c05b3515df3b1e3449f95d6df863a2c98ea5ad
David Batonda <99521514+dtonda8@users.noreply.github.com>
2022-12-21 08:01:46 +1100

    Added basic UI and tab features

--- src/index.js
+++ src/index.js
@@ -28,27 +28,46 @@ homeBtn.textContent = "Home";
 menuBtn.textContent = "Menu";
 aboutBtn.textContent = "About";
 
+homeBtn.classList.add('unhidden');
+homeBtn.classList.add('hidden');
+homeBtn.classList.add('hidden');
+
 btnsDiv.appendChild(homeBtn);
 btnsDiv.appendChild(menuBtn);
 btnsDiv.appendChild(aboutBtn);
+
+btnsDiv.style.textAlign = 'center'
+btnsDiv.style.padding = '10px'
 
 textDiv.appendChild(homeDiv);
 textDiv.appendChild(menuDiv);
 textDiv.appendChild(aboutDiv);
 
+textDiv.style.padding = "10px"
+
 contentDiv.appendChild(btnsDiv);
 contentDiv.appendChild(textDiv);
+
 
 function updatePage(e) {
     let divID = e.target.id + '-div';
     let divOfInterest = document.getElementById(divID);
 
     if (divOfInterest.classList[0] === 'hidden') {
-        let unhiddenDiv = document.querySelector(".unhidden");
+        let unhiddenDiv = document.querySelector("div.unhidden");
         unhiddenDiv.classList.remove('unhidden');
         unhiddenDiv.classList.add('hidden');
 
         divOfInterest.classList.remove('hidden');
         divOfInterest.classList.add('unhidden');
+
+        let unhiddenBtn = document.querySelector("button.unhidden");
+        unhiddenBtn.classList.add('hidden');
+        unhiddenBtn.classList.remove('unhidden');
+
+        e.target.classList.remove('hidden');
+        e.target.classList.add('unhidden');
     }
+
+
 }
